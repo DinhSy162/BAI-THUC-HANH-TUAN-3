b7c884a32a9941b4cae7c29c84fa392dc0f41bc4
Vo Van Phuong <phuongvovan30@gmail.com>
2025-09-10 00:08:23 +0700

    vo van phuong them bai 2

---
+++ TH_Tuan3.java
@@ -0,0 +1,16 @@
+
+package th_tuan3;
+import java.util.Scanner;
+
+public class TH_Tuan3 {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println(" Nhap ten: ");
+        String name = scanner.nextLine();
+        System.out.println(" Hello " + name);
+        
+    }
+    
+}
